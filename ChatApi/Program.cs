346b6a6dfbf0a68d7178633a9dadd0ca8aca3881
Blake Link <blakelinkd@gmail.com>
2023-12-05 17:03:19 -0600

    fixed out of date hostname

--- ChatApi/Program.cs
+++ ChatApi/Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: MyAllowSpecificOrigins, builder =>
     {
-        builder.WithOrigins("http://localhost:4200", "http://127.0.0.1:4200") // Add your Angular app's URL
+        builder.WithOrigins("https://fondly.ai") // Add your Angular app's URL
                .AllowAnyHeader()
                .AllowAnyMethod()
                .AllowCredentials();
