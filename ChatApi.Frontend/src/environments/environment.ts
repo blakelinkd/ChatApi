9729a298e3a2f6a961efeea335604bc785b5abca
Blake Link <blakelinkd@outlook.com>
2024-03-07 14:16:52 -0600

    Update API endpoints in environment.ts

--- ChatApi.Frontend/src/environments/environment.ts
+++ ChatApi.Frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    getEndpoint: 'https://fondly.ai/api/Chatbot/message/get',
-    postEndpoint: 'https://fondly.ai/api/Chatbot/message/post',
-    getByIdEndpoint: 'https://fondly.ai/api/Chatbot/message/getByThreadId'
+    getEndpoint: 'https://fondly.ai/Chatbot/message/get',
+    postEndpoint: 'https://fondly.ai/Chatbot/message/post',
+    getByIdEndpoint: 'https://fondly.ai/Chatbot/message/getByThreadId'
   };
